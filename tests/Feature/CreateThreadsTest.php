1991d6f3396f3850c3b2e31d893d9e32013e5c0f
Magic <meilunzhi@gmail.com>
2018-05-17 01:17:27 +0800

    添加用户测试文件

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -3,19 +3,36 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class CreateThreadsTest extends TestCase
 {
+	use DatabaseMigrations;
     /**
-     * A basic test example.
-     *
-     * @return void
+     * test
      */
-    public function testExample()
+
+    function gest_user_can_create_form_threads()
     {
-        $this->assertTrue(true);
+    	$this->expectException('Illuminate\Auth\AuthenticationException');
+	    $thread = factory('App\Thread')->make();
+	    $this->post('/threads',$thread->toArray());
+
     }
+
+
+    function an_authenticated_user_can_create_new_forum_threads()
+    {
+    	$this->actingAs(factory('App\User')->create());
+
+    	$thread = factory('App\Thread')->make();
+
+    	$this->post('/threads',$thread->toArray());
+
+    	$this->get($thread->path())
+    	     ->assertSee($thread->title)
+		     ->assertSee($thread->body);
+
+    }
+
 }
